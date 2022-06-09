2a33e6d34db013c825739eb9d13544f4c7ab553e
RizaldiFathan <brandalloka09@gmail.com>
2022-06-09 17:26:21 +0700

    link bootstrap

---
+++ studio.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" rel="stylesheet">
+    <!-- my css -->
+    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="coba.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Studio</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="POST">
+            <div class="row mt-5">
+                <div class="col-xl-6" style="color: red;">
+                    <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off">
+                    <label class="btn btn-secondary" for="option1" style="width: 50px;">A1</label>
+                    <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option2">A2</label>
+                    <input type="radio" class="btn-check" name="options" id="option3" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option3">A3</label>
+                    <input type="radio" class="btn-check" name="options" id="option4" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option4">A4</label>
+                    <input type="radio" class="btn-check" name="options" id="option5" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option5">A5</label>
+                    <input type="radio" class="btn-check" name="options" id="option6" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option6">A6</label>
+                    <input type="radio" class="btn-check" name="options" id="option7" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option7">A7</label>
+                    <br>
+                    <input type="radio" class="btn-check" name="options" id="option16" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option16">B1</label>
+                    <input type="radio" class="btn-check" name="options" id="option17" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option17">B2</label>
+                    <input type="radio" class="btn-check" name="options" id="option18" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option18">B3</label>
+                    <input type="radio" class="btn-check" name="options" id="option19" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option19">B4</label>
+                    <input type="radio" class="btn-check" name="options" id="option20" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option20">B5</label>
+                    <input type="radio" class="btn-check" name="options" id="option21" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option21">B6</label>
+                    <input type="radio" class="btn-check" name="options" id="option22" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option22">B7</label>
+
+                </div>
+                <div class="col-6">
+                    <input type="radio" class="btn-check" name="options" id="option9" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option9">A8</label>
+                    <input type="radio" class="btn-check" name="options" id="option10" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option10">A9</label>
+                    <input type="radio" class="btn-check" name="options" id="option11" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option11">A10</label>
+                    <input type="radio" class="btn-check" name="options" id="option12" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option12">A11</label>
+                    <input type="radio" class="btn-check" name="options" id="option13" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option13">A12</label>
+                    <input type="radio" class="btn-check" name="options" id="option14" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option14">A13</label>
+                    <input type="radio" class="btn-check" name="options" id="option15" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option15">A14</label>
+                    <br>
+                    <input type="radio" class="btn-check" name="options" id="option23" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option23">B8</label>
+                    <input type="radio" class="btn-check" name="options" id="option24" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option24">B9</label>
+                    <input type="radio" class="btn-check" name="options" id="option25" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option25">B10</label>
+                    <input type="radio" class="btn-check" name="options" id="option26" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option26">B11</label>
+                    <input type="radio" class="btn-check" name="options" id="option27" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option27">B12</label>
+                    <input type="radio" class="btn-check" name="options" id="option28" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option28">B13</label>
+                    <input type="radio" class="btn-check" name="options" id="option29" autocomplete="off">
+                    <label class="btn btn-secondary" style="width: 50px" for="option29">B14</label>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
--- upload.php
+++ upload.php
@@ -30,6 +30,7 @@ if (isset($_POST["upload"])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
   <!-- my css -->
