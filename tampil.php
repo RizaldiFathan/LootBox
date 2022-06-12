94e04f563900338ab096559dab3e9b84ca34e2e6
RizaldiFathan <brandalloka09@gmail.com>
2022-06-12 13:25:06 +0700

    tambah rating

--- tampil.php
+++ tampil.php
@@ -39,7 +39,7 @@ $movie = query("SELECT * FROM movies WHERE id_artikel = $id")[0];
                 <div class="card-body">
                     <h5 class="card-title text-uppercase"><?= $movie['judul_artikel']; ?></h5>
                     <p class="card-text"><?= $movie['deskripsi']; ?></p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text"><small class="text-muted"><?= $movie['rating']; ?></small><img src="img/star.png" style="margin-left: 5px;" alt=""></p>
                     <a href="studio.php" class="btn btn-primary col-2">Pesan</a>
                 </div>
             </div>
